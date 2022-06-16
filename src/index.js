14f96df78755dbe4e8e38eb18014819ac7742ae4
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-06-16 07:27:46 +0300

    Finished fetiching the images to the Dom

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+fetch(imgUrl)
+.then(res =>res.json())
+.then((images) => {console.log(images)
+   for (const item of images.message) {
+       const pictures = document.querySelector("#dog-image-container");
+       pictures.innerHTML += `<img src="${item}" alt=""> `
+   }
+})
+
+
+
+
